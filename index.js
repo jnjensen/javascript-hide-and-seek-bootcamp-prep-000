54f2b653bdf041e00cc4ebf554ff0d0c300115d8
James Jensen <jnjensen@live.com>
2020-02-07 03:42:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function increaseRankBy(n){
   console.log(lis)
  
   for (let i = 0; i < lis.length; i++) {
-    lis[i].innerHTML = (i+ n).toString();
+    lis[i].innerHTML = ((parseInt(lis[i].innerHTML))+ n);
     
   }
 }
