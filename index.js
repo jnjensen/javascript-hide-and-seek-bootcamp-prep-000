bcf7b6547d247dd3abe417b363795e7c1f2f757f
James Jensen <jnjensen@live.com>
2020-02-07 03:45:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function increaseRankBy(n){
   const upRank = document.getElementById('app').querySelectorAll('ul.ranked-list')
 
   for (let i = 0; i < upRank.length; i++){
-    upRank[i].innerHTML = ((parseInt(upRank[i].innerHTML)) + n);
+    upRank[i].innerHTML = ((parseInt(upRank[i].innerHTML)) + (parseInt(n)));
     
   }
 }
