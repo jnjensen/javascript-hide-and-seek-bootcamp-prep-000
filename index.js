07b75be5781e4af1821479457df2df8a4f245972
James Jensen <jnjensen@live.com>
2020-02-07 04:17:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.querySelector('div .target');
+}
+
+function increaseRankBy(n){
+  var list = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+
+  for (var i = 0; i < list.length; i++) {
+    list[i].innerHTML = parseInt(list[i].innerHTML) + parseInt(n);
+  }
+}
+
+function deepestChild(){
+ var child = document.getElementById('grand-node').querySelectorAll('div');
+  var i = child.length - 2;
+  var x = child[i].querySelector("div");
+  return x;
+}
+
+
+
+
+
