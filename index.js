74359f91e08faec82c4f2a6a793db4f8c9bdfdb4
James Jensen <jnjensen@live.com>
2020-02-07 03:46:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ function nestedTarget(){
 }
 
 function increaseRankBy(n){
-  var elements = document.querySelectorAll('ul.ranked-list li');
+  var list = document.querySelectorAll('ul.ranked-list li');
 
-  for (var i = 0; i < elements.length; i++) {
-    elements[i].innerHTML = parseInt(elements[i].innerHTML) + parseInt(n);
+  for (var i = 0; i < list.length; i++) {
+    list[i].innerHTML = parseInt(list[i].innerHTML) + parseInt(n);
     
   }
 }
