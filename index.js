34a7fef9ebc4e1a0a358917ef5c072538fd49a6a
James Jensen <jnjensen@live.com>
2020-02-07 04:04:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,13 @@ function deepestChild(){
   var child = document.getElementById('grand-node').querySelectorAll('div');
   var i = child.length
   
-  console.log(child[i])
+  let node = document.getElementById('grand-node')
+  let nextNode = node.children[0]
+
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0]
+  }
 }
 
 
