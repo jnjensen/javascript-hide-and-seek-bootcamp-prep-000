af5bc652f89f1dca37b1cdc7aff12169e1e50ca1
James Jensen <jnjensen@live.com>
2020-02-07 03:18:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ function getFirstSelector(selector){
 }
 
 function nestedTarget(){
-  return document.getElementById("nested").getElementByClass();
+  return document.getElementById("nested").getElementByClassname();
 }
